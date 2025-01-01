7f92c4d6bd58cdcc985c9aec4c1fe8b8fcdcfe3e
Ako Bate Emmanuel <95502815+Abate-Mony@users.noreply.github.com>
2025-01-01 20:12:09 +0000

    change url

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
-var url = 'http://192.168.43.68:5000'
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    // url = 'http://localhost:5000'
-    url = 'http://192.168.43.79:5000'
-} else {
-    url = "https://api.eagle-tranz.com/"
+
+let url = 'http://192.168.43.68:5000'; // Default URL, can be overwritten below
+
+if (process.env.NODE_ENV === 'development') {
+    // When in development environment, use the specific IP address
+    url = 'http://192.168.43.79:5000';
+} else if (process.env.NODE_ENV === 'production') {
+    // Use production server URL
+    url = 'https://api.eagle-tranz.com/';
 }
+
 const customFetch = axios.create({
     baseURL: url,
-
-    withCredentials: true
+    withCredentials: true,  // Ensures cookies are sent with the request
 });
 
 export default customFetch;
